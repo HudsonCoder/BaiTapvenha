111e5835f43c7b505b432be29d5870aab2d2feb0
Hudson <quangthien.itse@outlook.com>
2019-11-04 09:16:40 +0700

    Initial commit

---
+++ app/src/main/java/com/example/baitapvenha/Lop.java
@@ -0,0 +1,40 @@
+package com.example.baitapvenha;
+
+public class Lop {
+    private int idLop;
+    private String name;
+
+    public Lop(int idLop, String name) {
+        this.idLop = idLop;
+        this.name = name;
+    }
+
+    public Lop(String name) {
+        this.name = name;
+    }
+
+    public int getIdLop() {
+        return idLop;
+    }
+
+    public void setIdLop(int idLop) {
+        this.idLop = idLop;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Lop{" +
+                "idLop=" + idLop +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+}
